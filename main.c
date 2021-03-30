f4b7597d4d7a617f7495f8cd0d0352cc4f820e9c
Rita Mendes <49317803+SparklingRita@users.noreply.github.com>
2021-03-30 16:03:40 +0100

    modo0 sem restricao 1

--- main.c
+++ main.c
@@ -7,20 +7,20 @@ void utilizacao();
 void modos_jogo(int linhas, int colunas, int j, int p, int d, int n1, int n2, int n3, int n4, int n5, int n6, int n7, int n8);
 int random_number(int m, int M);
 void mostra_peca(char peca[9][3][3]);
-char tipo0(int i, int j);
-char tipo1(int i, int j);
-char tipo2(int i, int j);
-char tipo3(int i, int j);
-char tipo4(int i, int j);
-char tipo5(int i, int j);
-char tipo6(int i, int j);
-char tipo7(int i, int j);
-char tipo8(int i, int j);
+void tipo0(char tabuleiro[15][24], int i, int j);
+void tipo1(char tabuleiro[15][24], int i, int j);
+char tipo2(char tabuleiro[15][24], int i, int j);
+char tipo3(char tabuleiro[15][24], int i, int j);
+char tipo4(char tabuleiro[15][24], int i, int j);
+char tipo5(char tabuleiro[15][24], int i, int j);
+char tipo6(char tabuleiro[15][24], int i, int j);
+char tipo7(char tabuleiro[15][24], int i, int j);
+char tipo8(char tabuleiro[15][24], int i, int j);
 void init_boardgame(int linhas, int colunas);
-char peca_random(int i, int j);
+void peca_random(char tabuleiro[15][24], int i, int j);
 
 int main(int argc, char *argv[]){
-
+     srand(time(NULL));
     int linhas = 0, colunas = 0;
     init_boardgame(linhas, colunas);
     return 0;
@@ -79,12 +79,19 @@ void mostra_peca(char peca[9][3][3]){
     }
 }
 
-char tipo0(int i, int j){
+void tipo0(char tabuleiro[15][24], int i, int j){
+     int x,y;
      char variante0[3][3]={{'-','-','-'},{'-','-','-'},{'-','-','-'}};
-     return variante0[i][j];
+     for (x=0; x<3; x++) {
+        for (y=0; y<3; y++) {
+            tabuleiro[x+i][y+j]=variante0[x][y];
+        }
+     }
+
 }
 
-char tipo1(int i, int j){
+void tipo1(char tabuleiro[15][24], int i, int j){
+    int x,y;
     char variante1[9][3][3]={{{'1','-','-'},{'-','-','-'},{'-','-','-'}},
                              {{'-','1','-'},{'-','-','-'},{'-','-','-'}},
                              {{'-','-','1'},{'-','-','-'},{'-','-','-'}},
@@ -95,10 +102,19 @@ char tipo1(int i, int j){
                              {{'-','-','-'},{'-','-','-'},{'-','1','-'}},
                              {{'-','-','-'},{'-','-','-'},{'-','-','1'}}};
     int k = random_number(0, 8);
-    return variante1[k][i][j];
+
+    for (x=0; x<3; x++) {
+        for (y=0; y<3; y++) {
+            tabuleiro[x+i][y+j]=variante1[k][x][y];
+
+        }
+    }
+
+
 }
 
-char tipo2(int i, int j){
+char tipo2(char tabuleiro[15][24],int i, int j){
+      int x,y;
     char variante2[12][3][3] = {{{'2','2','-'},{'-','-','-'},{'-','-','-'}},
                                 {{'-','2','2'},{'-','-','-'},{'-','-','-'}},
                                 {{'-','-','-'},{'2','2','-'},{'-','-','-'}},
@@ -112,10 +128,16 @@ char tipo2(int i, int j){
                                 {{'-','-','2'},{'-','-','2'},{'-','-','-'}},
                                 {{'-','-','-'},{'-','-','2'},{'-','-','2'}}};
     int k = random_number(0, 11);
-    return variante2[k][i][j];
+    for (x=0; x<3; x++) {
+        for (y=0; y<3; y++) {
+            tabuleiro[x+i][y+j]=variante2[k][x][y];
+
+        }
+    }
 }
 
-char tipo3(int i, int j){
+char tipo3(char tabuleiro[15][24],int i, int j){
+    int x,y;
     char variante3[6][3][3] =  {{{'3','3','3'},{'-','-','-'},{'-','-','-'}},
                                 {{'-','-','-'},{'3','3','3'},{'-','-','-'}},
                                 {{'-','-','-'},{'-','-','-'},{'3','3','3'}},
@@ -123,46 +145,83 @@ char tipo3(int i, int j){
                                 {{'-','3','-'},{'-','3','-'},{'-','3','-'}},
                                 {{'-','-','3'},{'-','-','3'},{'-','-','3'}}};
     int k = random_number(0, 5);
-    return variante3[k][i][j];
+
+     for (x=0; x<3; x++) {
+        for (y=0; y<3; y++) {
+            tabuleiro[x+i][y+j]=variante3[k][x][y];
+
+        }
+    }
 }
 
-char tipo4(int i, int j){
+char tipo4(char tabuleiro[15][24],int i, int j){
+      int x,y;
     char variante4[4][3][3]=   {{{'4','4','-'},{'4','4','-'},{'-','-','-'}},
                                 {{'-','4','4'},{'-','4','4'},{'-','-','-'}},
                                 {{'-','-','-'},{'4','4','-'},{'4','4','-'}},
                                 {{'-','-','-'},{'-','4','4'},{'-','4','4'}}};
     int k = random_number(0, 3);
-    return variante4[k][i][j];
+     for (x=0; x<3; x++) {
+        for (y=0; y<3; y++) {
+            tabuleiro[x+i][y+j]=variante4[k][x][y];
+
+        }
+    }
 }
 
-char tipo5(int i, int j){
+char tipo5(char tabuleiro[15][24],int i, int j){
+    int x,y;
     char variante5[4][3][3]=   {{{'5','5','5'},{'-','5','-'},{'-','5','-'}},
                                 {{'5','-','-'},{'5','5','5'},{'5','-','-'}},
                                 {{'-','5','-'},{'-','5','-'},{'5','5','5'}},
                                 {{'-','-','5'},{'5','5','5'},{'-','-','5'}}};
     int k = random_number(0, 3);
-    return variante5[k][i][j];
-}
+      for (x=0; x<3; x++) {
+        for (y=0; y<3; y++) {
+            tabuleiro[x+i][y+j]=variante5[k][x][y];
 
-char tipo6(int i, int j){
+        }
+    }
+    }
+
+
+char tipo6(char tabuleiro[15][24],int i, int j){
+    int x,y;
     char variante6[4][3][3]=   {{{'-','6','-'},{'6','-','6'},{'6','6','6'}},
                                 {{'-','6','6'},{'6','-','6'},{'-','6','6'}},
                                 {{'6','6','6'},{'6','-','6'},{'-','6','-'}},
                                 {{'6','6','-'},{'6','-','6'},{'6','6','-'}}};
     int k = random_number(0, 3);
-    return variante6[k][i][j];
+      for (x=0; x<3; x++) {
+        for (y=0; y<3; y++) {
+            tabuleiro[x+i][y+j]=variante6[k][x][y];
+
+        }
+    }
 }
 
-char tipo7(int i, int j){
+char tipo7(char tabuleiro[15][24],int i, int j){
+    int x,y;
     char variante7[2][3][3]=   {{{'7','-','7'},{'7','7','7'},{'7','-','7'}},
                                 {{'7','7','7'},{'-','7','-'},{'7','7','7'}}};
     int k = random_number(0, 1);
-    return variante7[k][i][j];
+    for (x=0; x<3; x++) {
+        for (y=0; y<3; y++) {
+            tabuleiro[x+i][y+j]=variante7[k][x][y];
+
+        }
+    }
 }
 
-char tipo8(int i, int j){
+char tipo8(char tabuleiro[15][24],int i, int j){
+    int x,y;
     char variante8[3][3]={{'8','8','8'},{'8','-','8'},{'8','8','8'}};
-    return variante8[i][j];
+    for (x=0; x<3; x++) {
+        for (y=0; y<3; y++) {
+            tabuleiro[x+i][y+j]=variante8[x][y];
+
+        }
+    }
 }
 
 
@@ -170,7 +229,7 @@ void init_boardgame(int linhas, int colunas){
     char alfa[24]="ABCDEFGHIJKLMNOPQRSTUVWX";
     int nums[16]= {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
     char tabuleiro[15][24];
-    int i, j, max, a, b, c, d, K;
+    int i, j, max, a, b, c, d, k;
 
     do{
         printf("Insira as dimensoes (linhas)(colunas): "); //escolher as dimensoes do tabuleiro
@@ -178,18 +237,16 @@ void init_boardgame(int linhas, int colunas){
             if(linhas % 3 != 0 && colunas % 3 != 0 && linhas < 9 && linhas > 15 && colunas < 9 && colunas > 24){
                 printf("Dimensoes invalidas\n");
             }
+
     }while(linhas % 3 !=0 && colunas % 3 !=0 && linhas < 9 && linhas > 15 && colunas < 9 && colunas > 24);
 
     max = linhas;
+    printf("\n");
+    for(a = 0; a < linhas; a+=3){
+        for(b = 0; b < colunas; b+=3){
+            peca_random(tabuleiro,a,b);
 
-    for(a = 0; a < linhas; a++){
-        for(b = 0; b < colunas; b++){
 
-            for (i = 0; i < 3; i++){
-                for (j = 0; j < 3; j++){
-                    tabuleiro[i][j] = peca_random(i, j);
-                }
-            }
         }
     }
 
@@ -208,57 +265,73 @@ void init_boardgame(int linhas, int colunas){
 }
 
 /*
- * funcao: peca_random()
- *
- * recebe uma qualquer peca e retorna
- * uma aleatoria entre entre todas as pecas.
- *
+ funcao: peca_random()
+
+  recebe uma qualquer peca e retorna
+  uma aleatoria entre entre todas as pecas.
+
 */
-char peca_random(int i, int j){
-    char peca_ran[3][3];
+void peca_random(char tabuleiro[15][24], int i, int j){
+
+  /* char peca_ran[3][3];
+  /*  char pecas [10][3][3]; */
 
     switch(random_number(0, 8)){
     case 0:
-        peca_ran[i][j] = tipo0(i, j);
+        tipo0(tabuleiro,i,j);
         break;
     case 1:
-        peca_ran[i][j] = tipo1(i, j);
+        tipo1(tabuleiro,i,j);
         break;
     case 2:
-        peca_ran[i][j] = tipo2(i, j);
+        tipo2(tabuleiro,i,j);
         break;
     case 3:
-        peca_ran[i][j] = tipo3(i, j);
+        tipo3(tabuleiro,i,j);
         break;
     case 4:
-        peca_ran[i][j] = tipo4(i, j);
+        tipo4(tabuleiro,i,j);
         break;
     case 5:
-        peca_ran[i][j] = tipo5(i, j);
+        tipo5(tabuleiro,i,j);
         break;
     case 6:
-        peca_ran[i][j] = tipo6(i, j);
+        tipo6(tabuleiro,i,j);
         break;
     case 7:
-        peca_ran[i][j] = tipo7(i, j);
+        tipo7(tabuleiro,i,j);
         break;
     case 8:
-        peca_ran[i][j] = tipo8(i, j);
-    }
+        tipo8(tabuleiro,i,j);
+   }
 
-    return peca_ran[i][j];
+
+
+}
+
+/*int random_number(int m, int M, int count) {
+    count=2;
+    int i;
+    for (i=0;i<count;i++) {
+        int num=(rand() % (M - m + 1) + m);
+        printf("%d", num);
+    }
 }
 
 /*
- *  funcao: random_number()
- *
- *  recebe um minimo e um maximo e retorna
- *  um numero aleatorio entre esses dois valores.
- *
-*/
+  funcao: random_number()
+
+  recebe um minimo e um maximo e retorna
+  um numero aleatorio entre esses dois valores. */
+
+
 
 int random_number(int m, int M){
-    time_t t;
-    srand((unsigned) time(&t));
-    return rand() % (m - M - 1) + m;
-}
+    int n;
+
+    /*for (n=0; n<7; n++) { */
+    int i = rand() % (M - m + 1) + m;
+
+    return i;
+    }
+
