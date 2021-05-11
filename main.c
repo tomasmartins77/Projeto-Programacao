4b29206c989120825caea2c46aa63d323b33352e
Tomás Martins <80852872+tomasmartins77@users.noreply.github.com>
2021-05-11 15:44:41 +0100

    era uma vez o manel, ele tinha um chinelo

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <getopt.h>
 
 #define MAX_PALAVRAS_LINHAS 110
 #define MAX_CHAR 20
@@ -10,96 +8,62 @@
 typedef struct dados
 {
     char country[MAX_CHAR];
-    char country_code[MAX_CHAR];
+    char country_code[4];
     char continent[MAX_CHAR];
-    char population[MAX_CHAR];
-    char indicator[MAX_CHAR];
-    char weekly_count[MAX_CHAR];
-    char year_week[MAX_CHAR];
-    char rate_14_day[MAX_CHAR];
-    char cumulative_count[MAX_CHAR];
+    int population;
+    char indicator[7];
+    int weekly_count;
+    int year_week;
+    int rate_14_day;
+    int cumulative_count;
 
     struct dados *next;
 } dados_t;
 
-void menu(char **linhas, int *num_linhas, char *leitura_dados, char *ordenacao_dados, char *selecao_dados, char *restricao_dados, char *leitura_ficheiros, char *escrita_ficheiros);
-
-char **ler_ficheiro(char *nomeficheiro, int *linhas);
-char **ler_ficheiro_bin(char *nomeficheiro, int *linhas);
-char **linhas_continente(char **linhas_lidas, int *linhas, char *continente);
+char ** ler_ficheiro(char * nomeficheiro, int *linhas);
+char ** ler_ficheiro_bin(char * nomeficheiro, int *linhas);
+char** linhas_continente(char** linhas_lidas, int* linhas, char* continente);
 void liberta_memoria(char **linhas, int num_linhas);
-void escrever_ficheiro(char *nomeficheiro);
-void escrever_ficheiro_bin(char *nomeficheiro);
-void imprime_ficheiro(char **linhas_ficheiro, int *linhas);
+void escrever_ficheiro(char* nomeficheiro);
+void escrever_ficheiro_bin(char* nomeficheiro);
+void imprime_ficheiro(char** linhas_ficheiro, int* linhas);
 
 int main(int argc, char *argv[])
 {
-    char **linhas = NULL;
+    char ** linhas = NULL;
     int num_linhas = 0;
-    int opt;
-    opterr = 0;
-    char leitura_dados[MAX_PALAVRAS_LINHAS] = {0}, ordenacao_dados[MAX_PALAVRAS_LINHAS] = {0}, selecao_dados[MAX_PALAVRAS_LINHAS] = {0}, restricao_dados[MAX_PALAVRAS_LINHAS] = {0},
-         leitura_ficheiros[MAX_PALAVRAS_LINHAS] = {0}, escrita_ficheiros[MAX_PALAVRAS_LINHAS] = {0};
 
-    while ((opt = getopt(argc, argv, "hL:S:D:P:i:o:")) != -1)
-    {
-        switch (opt)
-        {
-        case 'L':
-            sscanf(optarg, "%s", leitura_dados);
-            break;
-        case 'S':
-            sscanf(optarg, "%s", ordenacao_dados);
-            break;
-        case 'D':
-            sscanf(optarg, "%s", selecao_dados);
-            break;
-        case 'P':
-            sscanf(optarg, "%s", restricao_dados);
-            break;
-        case 'i':
-            sscanf(optarg, "%s", leitura_ficheiros);
-            break;
-        case 'o':
-            sscanf(optarg, "%s", escrita_ficheiros);
-            break;
-        default: /* opcoes invalidas */
-            printf("Erro na leitura");
-            return EXIT_FAILURE;
-            break;
-        }
-    }
-    linhas = ler_ficheiro("covid19_w_t01.csv", &num_linhas);
 
-    linhas = linhas_continente(linhas, &num_linhas, "Africa");
-    imprime_ficheiro(linhas, &num_linhas);
-    liberta_memoria(linhas, num_linhas);
+
+
     return 0;
 }
 
-char **ler_ficheiro(char *nomeficheiro, int *linhas)
+
+
+char ** ler_ficheiro(char * nomeficheiro, int *linhas)
 {
-    FILE *ficheiro;
+    FILE * ficheiro;
     char buffer[MAX_PALAVRAS_LINHAS];
-    char **linha_lida;
+    char ** linha_lida;
     ficheiro = fopen(nomeficheiro, "r");
 
-    if (ficheiro == NULL)
+    if(ficheiro == NULL)
     {
         printf("Erro a abrir ficheiro\n");
         exit(EXIT_FAILURE);
     }
     *linhas = 0;
-    while (fgets(buffer, MAX_PALAVRAS_LINHAS, ficheiro) != NULL)
+    while(fgets(buffer, MAX_PALAVRAS_LINHAS, ficheiro) != NULL)
     {
-        if (*linhas != 0)
+        if(*linhas != 0)
         {
             int length = strlen(buffer);
-            if (buffer[length - 1] == '\n')
+            if(buffer[length - 1] == '\n')
                 buffer[length - 1] = '\0';
-            if (*linhas == 1)
+            if(*linhas == 1)
             {
-                if ((linha_lida = (char **)malloc(sizeof(char **))) == NULL)
+                if((linha_lida = (char**)malloc(sizeof(char*))) == NULL)
                 {
                     printf("Erro na alocacao de memoria\n");
                     exit(EXIT_FAILURE);
@@ -107,13 +71,14 @@ char **ler_ficheiro(char *nomeficheiro, int *linhas)
             }
             else
             {
-                if ((linha_lida = (char **)realloc(linha_lida, *linhas * sizeof(char *))) == NULL)
+                if((linha_lida = (char**)realloc(linha_lida, *linhas * sizeof(char*))) == NULL)
                 {
                     printf("Erro no realloc\n");
                     exit(EXIT_FAILURE);
                 }
+
             }
-            linha_lida[(*linhas) - 1] = (char *)malloc(sizeof(char *) * length + 1);
+            linha_lida[(*linhas) - 1] = (char*)malloc(sizeof(char*) * length + 1);
             strcpy(linha_lida[(*linhas) - 1], buffer);
         }
         (*linhas)++;
@@ -123,28 +88,101 @@ char **ler_ficheiro(char *nomeficheiro, int *linhas)
     return linha_lida;
 }
 
-char **ler_ficheiro_bin(char *nomeficheiro, int *linhas)
+dados_t  cria_lista()
 {
-    FILE *ficheiro;
+    FILE* ficheiro;
+    char* buffer = NULL;
+    dados_t* root = malloc(sizeof(dados_t));
+    ficheiro = fopen("covid19_w_t01.csv", "r");
+
+    if(ficheiro == NULL)
+    {
+        printf("Erro a abrir ficheiro\n");
+        exit(EXIT_FAILURE);
+    }
+
+    (root) = NULL;
+    int linhas = 0;
+    while(fgets(buffer, MAX_PALAVRAS_LINHAS, ficheiro) != NULL)
+    {
+        if(linhas == 0)
+        {
+            continue;
+        }
+        int length = strlen(buffer);
+        if(buffer[length - 1] == '\n')
+            buffer[length - 1] = '\0';
+
+
+        if((buffer = (char*)malloc(MAX_PALAVRAS_LINHAS * sizeof(char*))) == NULL)
+        {
+            printf("Erro na alocacao de memoria\n");
+            exit(EXIT_FAILURE);
+        }
+        for(int i = 0; i < 9; i++)
+        {
+            char* token = strtok(buffer, ",");
+            switch(i)
+            {
+            case 0:
+                strcpy(root->country, token);
+                break;
+            case 1:
+                strcpy(root->country_code, token);
+                break;
+            case 2:
+                strcpy(root->continent, token);
+                break;
+            case 3:
+                root->population = token;//ver que ainda e int e nao char
+                break;
+            case 4:
+                strcpy(root->indicator, token);
+                break;
+            case 5:
+                root->weekly_count = token;
+                break;
+            case 6:
+                root->year_week = token;
+                break;
+            case 7:
+                root->rate_14_day = token;
+                break;
+            case 8:
+                root->cumulative_count = token;
+                break;
+            }
+        }
+        root = root->next;
+        free(buffer);
+        linhas++;
+    }
+}
+
+
+/*
+char ** ler_ficheiro_bin(char * nomeficheiro, int *linhas)
+{
+    FILE * ficheiro;
     char buffer[MAX_PALAVRAS_LINHAS];
 
-    char **linha_lida;
+    char ** linha_lida;
     ficheiro = fopen(nomeficheiro, "rb");
 
-    if (ficheiro == NULL)
+    if(ficheiro == NULL)
     {
         printf("Erro a abrir ficheiro\n");
         exit(EXIT_FAILURE);
     }
     *linhas = 1;
-    while (fgets(buffer, MAX_PALAVRAS_LINHAS, ficheiro) != NULL)
+    while(fgets(buffer, MAX_PALAVRAS_LINHAS, ficheiro) != NULL)
     {
         int length = strlen(buffer);
-        if (buffer[length - 1] == '\n')
+        if(buffer[length - 1] == '\n')
             buffer[length - 1] = '\0';
-        if (*linhas == 1)
+        if(*linhas == 1)
         {
-            if ((linha_lida = (char **)malloc(sizeof(char **))) == NULL)
+            if((linha_lida = (char**)malloc(sizeof(char*))) == NULL)
             {
                 printf("Erro na alocacao de memoria\n");
                 exit(EXIT_FAILURE);
@@ -152,28 +190,29 @@ char **ler_ficheiro_bin(char *nomeficheiro, int *linhas)
         }
         else
         {
-            if ((linha_lida = (char **)realloc(linha_lida, *linhas * sizeof(char *))) == NULL)
+            if((linha_lida = (char**)realloc(linha_lida, *linhas * sizeof(char*))) == NULL)
             {
                 printf("Erro no realloc\n");
                 exit(EXIT_FAILURE);
             }
+
         }
-        linha_lida[(*linhas) - 1] = (char *)malloc(sizeof(char *) * length + 1);
+        linha_lida[(*linhas) - 1] = (char*)malloc(sizeof(char*) * length + 1);
         strcpy(linha_lida[(*linhas) - 1], buffer);
         (*linhas)++;
     }
     (*linhas)--;
     fclose(ficheiro);
     return linha_lida;
-}
+}*/
 
-void imprime_ficheiro(char **linhas_ficheiro, int *linhas)
+void imprime_ficheiro(char** linhas_ficheiro, int* linhas)
 {
     int i, j;
 
-    for (i = 0; i < *linhas; i++)
+    for(i=0; i<*linhas; i++)
     {
-        for (j = 0; j < strlen(linhas_ficheiro[i]); j++)
+        for(j = 0; j < strlen(linhas_ficheiro[i]); j++)
         {
             printf("%c", linhas_ficheiro[i][j]);
         }
@@ -181,23 +220,23 @@ void imprime_ficheiro(char **linhas_ficheiro, int *linhas)
     }
 }
 
-void escrever_ficheiro(char *nomeficheiro)
+void escrever_ficheiro(char* nomeficheiro)
 {
     char buffer[MAX_PALAVRAS_LINHAS];
 
-    FILE *ficheiro = fopen("covid19_w_t01", "w");
-    if (ficheiro == NULL)
+    FILE* ficheiro = fopen("covid19_w_t01", "w");
+    if(ficheiro == NULL)
     {
         printf("Erro a abrir ficheiro");
         exit(EXIT_FAILURE);
     }
     printf("Para terminar de escrever escrever: EOF\n---------------------------------------\n");
-    while (fgets(buffer, MAX_PALAVRAS_LINHAS, stdin) != NULL)
+    while(fgets(buffer, MAX_PALAVRAS_LINHAS, stdin) != NULL)
     {
-        if (strstr(buffer, "EOF") != 0)
+        if(strstr(buffer,"EOF") != 0)
         {
             int length = strlen(buffer);
-            for (int i = 0; i < length - 4; i++)
+            for(int i = 0; i < length - 4; i++)
                 fputc(buffer[i], ficheiro);
             break;
         }
@@ -206,31 +245,45 @@ void escrever_ficheiro(char *nomeficheiro)
     fclose(ficheiro);
 }
 
-void escrever_ficheiro_bin(char *nomeficheiro)
+char* string_tok(char* linha, int* linhas_totais)
 {
-    char buffer[MAX_PALAVRAS_LINHAS];
+    char* token;
+    char* linhas_novas;
 
-    FILE *ficheiro = fopen(nomeficheiro, "wb");
-    if (ficheiro == NULL)
+    token = strtok(linha, ",");
+
+    while (token != NULL)
     {
-        printf("Erro a abrir ficheiro");
-        exit(EXIT_FAILURE);
+        token = strtok(NULL, ",");
     }
-    printf("Para terminar de escrever escrever: EOF\n---------------------------------------\n");
-    while (fgets(buffer, MAX_PALAVRAS_LINHAS, stdin) != NULL)
-    {
-        if (strstr(buffer, "EOF") != 0)
-        {
-            int length = strlen(buffer);
-            for (int i = 0; i < length - 4; i++)
-                fputc(buffer[i], ficheiro);
-            break;
-        }
-        fputs(buffer, ficheiro);
-    }
-    fclose(ficheiro);
+    return linhas_novas;
 }
 /*
+void escrever_ficheiro_bin(char* nomeficheiro)
+{
+    char buffer[MAX_PALAVRAS_LINHAS];
+
+    FILE* ficheiro = fopen(nomeficheiro, "wb");
+    if(ficheiro == NULL)
+    {
+        printf("Erro a abrir ficheiro");
+        exit(EXIT_FAILURE);
+    }
+    printf("Para terminar de escrever escrever: EOF\n---------------------------------------\n");
+    while(fgets(buffer, MAX_PALAVRAS_LINHAS, stdin) != NULL)
+    {
+        if(strstr(buffer,"EOF") != 0)
+        {
+            int length = strlen(buffer);
+            for(int i = 0; i < length - 4; i++)
+                fputc(buffer[i], ficheiro);
+            break;
+        }
+        fputs(buffer, ficheiro);
+    }
+    fclose(ficheiro);
+}
+
 char** linhas_continente(char** linhas_lidas, int* linhas, char* continente)
 {
     int i;
@@ -253,12 +306,75 @@ char** linhas_continente(char** linhas_lidas, int* linhas, char* continente)
     }
 
     return novas_linhas;
-}*/
+}
 
+void new_node(dados_t** root, char* string)
+{
+    dados_t* new_node = (char*)malloc(sizeof(dados_t));
+    if(new_node == NULL)
+    {
+        printf("Erro a alocar novo node");
+        exit(EXIT_FAILURE);
+    }
+    new_node->next = NULL;
+    new_node->country = string;
+
+    if(*root == NULL)
+    {
+        *root = new_node;
+        return;
+    }
+
+    dados_t* curr = *root;
+    while(curr->next != NULL)
+    {
+        curr = curr->next;
+    }
+    curr->next = new_node;
+}
+void deallocate_node(node_t** root)
+{
+    node* curr = *root;
+    while(curr != NULL)
+    {
+        node* aux = *curr;
+        curr = curr->next;
+        free(aux);
+    }
+    *root = NULL;
+}
+
+insert_beginning(dados_t** root, char* linha)
+{
+    dados_t* new_node = malloc(sizeof(dados_t));
+    if(new_node == NULL)
+    {
+        printf("Erro na alocacao");
+        exit(-1);
+    }
+    new_node->? = linha;
+    new_node->next = *root;
+
+    *root = new_node;
+}
+
+void insert_after(dados_t* node, char* string)
+{
+    dados_t* new_node = malloc(sizeof(char*));
+    if(new_node == NULL)
+    {
+        printf("Erro na alocacao");
+        exit(-1);
+    }
+    new_node->? = string;
+    new_node->next = node->next;
+    node->next = new_node;
+}
+*/
 void liberta_memoria(char **linhas, int num_linhas)
 {
     int i;
-    for (i = 0; i < num_linhas; i++)
+    for(i = 0; i < num_linhas; i++)
     {
         free(linhas[i]);
     }
